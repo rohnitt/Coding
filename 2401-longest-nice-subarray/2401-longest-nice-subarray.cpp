b3bf50dff02af967f012032cc4f37934e5bb58ca
rohnitt <44682451+rohnitt@users.noreply.github.com>
2022-09-05 20:09:14 +0530

    Update 2401-longest-nice-subarray.cpp

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        int num = 0;
+        int num = 0; //contains set bit of all numbers in array
         int j =0;
         int ans = INT_MIN;
         for(int i=0;i<nums.size();i++)
         {
             while ( (num & nums[i]) != 0) 
             {
-               num^=nums[j++];       
+                // keep removing numbers from back unless problem is solved
+                // Solution ---> and of all numbers in subarray should be zero
+               num^=nums[j++];     // XoR to remove where bits are set   
             }  
-             num|= nums[i];
-            ans = max(ans,i-j+1);
+            // if nums & nums[i]==0;
+             num|= nums[i]; // add number
+            ans = max(ans,i-j+1); // max length
         }
         return ans;
     }
