5a3b6de5ee521dae90bfbab02f142b44dd62407f
rohnitt <44682451+rohnitt@users.noreply.github.com>
2022-09-06 15:27:17 +0530

    Time: 3 ms (71.58%), Space: 9.3 MB (5.18%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -15,7 +15,7 @@ public:
         if (!root) return root;
         root->left = pruneTree(root->left);
         root->right = pruneTree(root->right);
-        if(!root->left && !root->right && !root->val) 
+        if(!root->left && !root->right && root->val==0) 
         {
             
                delete root;
@@ -24,30 +24,7 @@ public:
         return root;
     }
 };
-// In case you want to free the deleted root to avoid memory leak. You can change the last line to
 
-// if (!root->left && !root->right && !root->val) {
-//     // delete will delete the memory of the pointer of root
-// 	delete root;
-// 	return nullptr;
-// } else {
-// 	return root;
-// }
-// Python
-
-// # Definition for a binary tree node.
-//         if(root==NULL)
-//             return root;
-//         TreeNode* l=pruneTree(root->left);
-//         TreeNode* r=pruneTree(root->right);
-//         //if(!root->left and !root->right and root->val==0) return NULL;
-//         if(root->left->val==0 and root->val==1 and root!=NULL)
-//             root->left=NULL;
-//         if(root->right->val==0 and root->val==1 and root!=NULL)
-//             root->right=NULL;
-//         return root;
-//     }
-// };
 // // if(root!=NULL){
 // //             root->left = pruneTree(root->left);
 // //             root->right = pruneTree(root->right);
