7210a2e941b18930a0ac8f8704ce1a2bdd0ba66d
rohnitt <44682451+rohnitt@users.noreply.github.com>
2022-09-09 21:19:19 +0530

    Time: 1443 ms (9.80%), Space: 51.4 MB (5.44%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJumpUtil(vector<int>& nums,int i,vector<int>&dp)
+    {
+        if(i>=nums.size()-1)return true; // if crosses the size of array then can jump so true
+        if(dp[i]!=-1)return dp[i]; 
+        if(nums[i]==0)return dp[i]=false; // if the ith index value is zero no further jump so false
+        for(int j=1;j<=nums[i];j++)
+        {
+            if(canJumpUtil(nums,i+j,dp))
+                return dp[i]=true;
+        }
+        return dp[i]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return canJumpUtil(nums,0,dp);
+    }
+};
