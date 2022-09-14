0a13b7b72cb931b18d8573ae573290795fcd9a23
rohnitt <44682451+rohnitt@users.noreply.github.com>
2022-09-14 19:50:04 +0530

    Time: 712 ms (31.57%), Space: 181.9 MB (51.95%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+int count =0;
+vector<int> digits{0,0,0,0,0,0,0,0,0,0}; //for counting the frequency
+    void pseudo_count1(TreeNode* root,vector<int>&digits,int& count)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    digits[root->val]++;
+    pseudo_count1(root->left,digits,count);
+    pseudo_count1(root->right,digits,count);
+    
+    if(root->left==NULL && root->right==NULL)
+    {
+        int odd_count =0;
+        for(int i=0;i<10;i++)
+        {
+            if(digits[i]%2!=0)
+            {
+                odd_count++; //counting the odd digits
+            }
+        }
+        if(odd_count==0 || odd_count==1)
+        {
+            count++;
+        }
+    }
+    digits[root->val]--;
+    
+}
+int pseudoPalindromicPaths(TreeNode* root){
+    pseudo_count1(root,digits,count);
+     return count;
+    
+}
+
+
+};
