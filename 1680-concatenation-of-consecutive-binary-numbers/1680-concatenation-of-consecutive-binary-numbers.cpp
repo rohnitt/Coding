5376862d126370e10a16e54ed68c1603a85f30aa
rohnitt <44682451+rohnitt@users.noreply.github.com>
2022-09-24 00:41:45 +0530

    Time: 147 ms (48.37%), Space: 6 MB (22.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
-    // the idea is to use bit manipulation to set the current number based on the previous number
-    // for example, 
-    // n = 1, ans = 0b1
-    // n = 2 (10), we need to shift 2 bits of the previous ans to the left and add `n`
-    // i.e. 1 -> 100 (shift 2 bits to the left) -> 110 (set `10`). ans = 0b110
-    // n = 3 (11), we need to shift 2 bits of the previous ans to the left and add `n` 
-    // i.e 110 -> 11000 (shift 2 bits to the left) -> 11011 (set `11`). ans = 0b11011
-    // n = 4 (100), we need to shift 3 bits of the previous ans to the left and add `n`
-    // i.e. 11011 -> 11011000 (shift 3 bits to the left) -> 11011100 (set `100). ans = 0b11011100
-    // so now we can see a pattern here
-    // we need to shift `l` bits of the previous ans to the left and add the current `i` 
-    // how to know `l`? it is not difficult to see `x` only increases when we meet power of 2
     int concatenatedBinary(int n) {
-        // `size` is the bit length to be shifted
-        int Mod= 1e9 + 7, size = 0;
-        // use long here as it potentially could overflow for int
-        long ans = 0;
-        for (int i = 1; i <= n; i++) {
-            // i & (i - 1) means removing the rightmost set bit
-            // e.g. 100100 -> 100000
-            //      000001 -> 000000
-            //      000000 -> 000000
-            // after removal, if it is 0, then it means it is power of 2
-            // as all power of 2 only contains 1 set bit
-            // if it is power of 2, we increase the bit length `size`
-            if ((i & (i - 1)) == 0) size += 1;
-            // (ans << l) means shifting the orginal answer `l` bits to th left
-            // (x | i) means  using OR operation to set the bit
-            // e.g. 0001 << 3 = 0001000
-            // e.g. 0001000 | 0001111 = 0001111
-            ans = ((ans << size) | i) % Mod;
+        long long int prev = 0;
+        int i=1;
+        while(i<=n)
+        {
+            prev = ((prev << (1+int(log2(i))))%1000000007 + i)%1000000007; // previous value will be left shifted by current digit of (i) and add current 'i'--i.e  (prev << digitof(i) + i)
+           // digit of i= 1+log2(i);
+            i+=1;
         }
-        return ans;
+        return prev;
     }
 };
